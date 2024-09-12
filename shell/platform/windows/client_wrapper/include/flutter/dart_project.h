5b6218c44a8116967ab6b8bfaf30b97c44423945
songsongcai <songsongcai@tencent.com>
2024-09-12 14:09:20 +0800

    feat: dart Project with soft render arg

--- shell/platform/windows/client_wrapper/include/flutter/dart_project.h
+++ shell/platform/windows/client_wrapper/include/flutter/dart_project.h
@@ -56,6 +56,8 @@ class DartProject {
     dart_entrypoint_ = entrypoint;
   }
 
+  void set_flutter_use_soft_render(bool val) { flutter_use_soft_render_ = val; }
+
   // Returns the Dart entrypoint.
   const std::string& dart_entrypoint() const { return dart_entrypoint_; }
 
@@ -83,6 +85,9 @@ class DartProject {
   const std::wstring& assets_path() const { return assets_path_; }
   const std::wstring& icu_data_path() const { return icu_data_path_; }
   const std::wstring& aot_library_path() const { return aot_library_path_; }
+  const bool flutter_use_soft_render() const {
+    return flutter_use_soft_render_;
+  }
 
   // The path to the assets directory.
   std::wstring assets_path_;
@@ -95,6 +100,8 @@ class DartProject {
   std::string dart_entrypoint_;
   // The list of arguments to pass through to the Dart entrypoint.
   std::vector<std::string> dart_entrypoint_arguments_;
+  // Flutter use soft render
+  bool flutter_use_soft_render_;
 };
 
 }  // namespace flutter
